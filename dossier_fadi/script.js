3225d23fda393d830a16190265168aff6099db67
fadisafar83 <fadisafar@hotmail.com>
2021-10-07 16:13:43 +0200

    second commit

--- dossier_fadi/script.js
+++ dossier_fadi/script.js
@@ -1,11 +1,11 @@
 const Users = {
-    "nom":[],
-    "prenom": [],
-    "email":[],
-    "motDePasse":[],
-    "phone":[],
-    "adresse":[],
-    "codePostal":[]
+    "nom":["safar"],
+    "prenom": ["fadi"],
+    "email":["fadi@hotmail.com"],
+    "motDePasse":["fadi"],
+    "phone":["09999999"],
+    "adresse":["12 rue de mais"],
+    "codePostal":["93209"]
 }
 
 $(document).ready(function(){
@@ -38,10 +38,13 @@ $("#inscrire").click(function(){
     console.log(Users)
 })
 
+
 $("#connecter").click(function(){
 
     let newemail= document.getElementById("exampleInputEmail1").value
     let newpassword= document.getElementById("exampleInputPassword1").value
+
+
     
 
     for (let i = 0; i < Users.email.length && i < Users.motDePasse.length ; i++ ) {
@@ -63,7 +66,68 @@ $("#connecter").click(function(){
 })
 
 
+$(document).ready(function(){
+    $("#send-message").click(function(){
+    let firstName = $("#recipient-Firstname").val()
+    let nickName = $("#recipient-Nickname").val()
+    let mail = $("#recipient-email").val()
+    let message = $("#message-text").val()
+
+        if(firstName && nickName && mail && message !== "") {
+    
+              alert("Nous avons réçu votre message")
+              location.reload();
+                    
+                }else{
+    
+                alert("veuillez ressaisir vos données!!")
+            }        
+    })
+
+
+    $("#fermer").click(function(){
+    
+              location.reload(); 
+    })
+})
+       
 
 
 
+$(document).ready(function(){
+    $("#valid-modif").click(function(){
+    let nickName_1 = $("#recipient-Nickname_1").val()
+    let firstName_1 = $("#recipient-Firstname_1").val()
+    let mail_1= $("#recipient-email_1").val()
+    let password_1 = $("#recipient-password_1").val()
+    let mobile_1 = $("#recipient-mobile_1").val()
+    let adresse_1 = $("#inputAdresse_1").val()
+    let codePostal_1 = $("#inputGroupSelect01_1").val()
 
+        if(nickName_1 && firstName_1 && mail_1 && password_1 && mobile_1 && adresse_1 && codePostal_1 !== "") {
+
+
+            Users.nom.push(nickName_1)
+            Users.prenom.push(firstName_1)
+            Users.email.push(mail_1)
+            Users.motDePasse.push(password_1)
+            Users.phone.push(mobile_1)
+            Users.adresse.push(adresse_1)
+            Users.codePostal.push(codePostal_1)
+    
+              alert("Nous avons modifié vos information")
+              console.log(Users)
+              location.reload();
+                    
+                }else{
+    
+                alert("veuillez ressaisir vos données!!")
+            }        
+    })
+
+
+    $("#annuler").click(function(){
+    
+              location.reload(); 
+    })
+})
